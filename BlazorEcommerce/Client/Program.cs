4ba6d4c603542bcba8fc083f509658d0171491d1
Henry Ihenacho <henrychris2003@gmail.com>
2022-05-19 15:59:15 +0100

    removed redundant service

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -12,7 +12,6 @@ using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
-using BlazorEcommerce.Client.Services.ProductTypeService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
